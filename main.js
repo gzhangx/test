bcaa296b2bbb498555316e63065e0422d9460cee
Gang Zhang <gang.zhang@pps.io>
2017-01-01 23:15:35 -0500

    adding react router

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
 import Menu from './components/topmenu/index.jsx';
 import DataProvider from './DataProvider.jsx';
 import api from './api.js';
 
+function NoCmp(data) {
+    return ()=><div>{data || 'NoCmp'}</div>
+}
 class MainPage extends React.Component {
     constructor(props) {
         super(props);
@@ -14,9 +18,9 @@ class MainPage extends React.Component {
     }
 
     menuChanged(val) {
+        browserHistory.push('/'+val.key);
         this.setState({name: val.key});
         api.getReadList().then(res=> {
-            console.log(res);
            this.setState({readList: res.data});
         });
     }
@@ -24,6 +28,15 @@ class MainPage extends React.Component {
         return <DataProvider state={this.state} api={api}>
             <div>
                 <Menu menuChange={this.menuChanged}></Menu>
+                <Router history={browserHistory}>
+                    <Route path="/" component={NoCmp('default')}>
+                        <Route path="/login" component={NoCmp('login')}/>
+                        <Route path="/users" component={NoCmp('users')}>
+                            <Route path="/user/:userId" component={NoCmp('usersid')}/>
+                        </Route>
+                        <Route path="*" component={NoCmp('star')}/>
+                    </Route>
+                </Router>
                 <div>{this.state.name}</div>
                 <ul>
                 {
