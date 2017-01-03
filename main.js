d82d92260da6155e9d4cde7af11dc99a6839126d
Gang Zhang <gang.zhang@pps.io>
2017-01-02 19:11:21 -0500

    use good history,

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { Router, Route, Link, browserHistory } from 'react-router'
+import {createHashHistory} from 'history';
 import Menu from './components/topmenu/index.jsx';
 import DataProvider from './DataProvider.jsx';
 import api from './api.js';
 import Login from './components/login/index.jsx';
 
-function NoCmp(data) {
-    return ()=><div>{data || 'NoCmp'}</div>
-}
+
 class MainPage extends React.Component {
     constructor(props) {
         super(props);
+        this.history = createHashHistory();
+        this.history.listen(location => {
+            console.log(location);
+        });
         this.menuChanged = this.menuChanged.bind(this);
         this.state = {
             name: 'state name'
@@ -19,8 +21,12 @@ class MainPage extends React.Component {
     }
 
     menuChanged(val) {
-        browserHistory.push('/'+val.key);
-        this.setState({name: val.key});
+        //BrowserHistory.Push('/'+val.key);
+        this.history.replace({
+            pathname: val.key,
+            search: '',
+        });
+        this.setState({pathname: val.key});
         if (val.key == 'login') {
             this.setState({readList: null});
         }else
@@ -28,30 +34,33 @@ class MainPage extends React.Component {
            this.setState({readList: res.data});
         });
     }
+    route(urlHash) {
+        switch (urlHash) {
+            case 'login':
+                return <Login/>;
+            default:
+                return <ul>
+                    {
+                        this.state.readList && this.state.readList.map(l=>
+                            <li>{l.title}</li>
+                        )
+                    }
+                </ul>;
+        }
+    }
+
     render() {
         return <DataProvider state={this.state} api={api}>
             <div>
                 <Menu menuChange={this.menuChanged}></Menu>
                 <div>{this.state.name}</div>
-                <ul>
-                {
-                    this.state.readList && this.state.readList.map(l=>
-                        <li>{l.title}</li>
-                    )
-                }
-                </ul>
-                <Link  to='login'>Login</Link>
-                {this.props.children}
+                {this.route(this.state.pathname)}
             </div>
         </DataProvider>;
     }
 }
 
-render(<Router history={browserHistory}>
-        <Route path="/" component={MainPage}>
-            <Route path="login" component={Login}/>
-        </Route>
-    </Router>
+render(<MainPage/>
     , document.getElementById('root'));
 
 
