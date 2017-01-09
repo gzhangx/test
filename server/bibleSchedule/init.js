53220518aa0f1b6835d2260b6442928c0aefdc98
Gang Zhang <gang.zhang@pps.io>
2017-01-08 21:27:59 -0500

    adding blank view

--- server/bibleSchedule/init.js
+++ server/bibleSchedule/init.js
@@ -1,12 +1,26 @@
-const schedule = require('./bibleSchedule');
 
+const calcLib = require('./scheduleCalcLib');
+const curSchedule = calcLib.schedule.getScheduleByOffset(new Date(), 0);
+console.log(curSchedule);
+const reduce = require('lodash/reduce');
+
+function convertWeek(which) {
+    switch (which)
+    {
+        case 0: return '一';
+        case 1: return '二';
+        case 2: return '三';
+        case 3: return '四';
+        case 4: return '五';
+        case 5: return '六';
+        case 6: return '日';
+    }
+}
 function setupRoutes(app) {
-    app.get('/schedule', (req, res) => {
-        res.jsonp({
-            curweek: schedule.res.CurWeekAry,
-            scheduleStarts: schedule.res.scheduleStarts,
-            curSchedule: schedule.res.curSchedule
-        });
+    app.get('/blankView', (req, res) => {
+        let str = ''
+        curSchedule.curWeekView.map((r, i)=>{ str = `${str}${r.date.substr(5)}(${convertWeek(i)}) ${r.verse}<br>` });
+        res.send(200, str);
     });
 }
 
